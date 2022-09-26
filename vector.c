bed3dea9bd391ab8952597e84db980930bffdfe4
licktheroom <77417158+licktheroom@users.noreply.github.com>
2022-09-25 20:13:06 -0400

    Update vector.c

--- vector.c
+++ vector.c
@@ -1,6 +1,6 @@
 // Copyright (C) 2022  licktheroom //
 
-#include "include/vector.h"
+#include "include/engine.h"
 
 #include <math.h>
 #include <stdbool.h>
@@ -13,18 +13,10 @@ int abs(int a)
     return a;
 }
 
-unsigned int VEC_dist(vec2 a, vec2 b)
+uint v2_distance(vec2 a, vec2 b)
 {
     int x = pow(abs(a.x-b.x), 2);
 	int y = pow(abs(a.y-b.y), 2);
 
 	return sqrt(x+y);
-}
-
-bool VEC_eq(vec2 a, vec2 b)
-{
-    if(a.x == b.x && a.y == b.y)
-        return true;
-
-    return false;
 }
