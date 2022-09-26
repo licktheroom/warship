125c1f3a7f123bc38b09c9784c63e63e10a60688
licktheroom <77417158+licktheroom@users.noreply.github.com>
2022-09-25 20:08:03 -0400

    Update level_data.h

--- include/level_data.h
+++ include/level_data.h
@@ -4,36 +4,34 @@
 #define LEVEL_DATA_HEADER
 
 #include <stdbool.h>
+#include "engine.h"
 
-#include "enums.h"
-#include "vector.h"
-
-struct ship_level_data
+struct t_ship_level_data
 {
     vec2 pos;
     Ship_type type;
     bool directions[4];
 };
 
-struct level_data
+struct t_level_data
 {
     int blue_ship_n;
     int red_ship_n;
 
-    struct ship_level_data blue_ships[25];
-    struct ship_level_data red_ships[25];
+    struct t_ship_level_data blue_ships[25];
+    struct t_ship_level_data red_ships[25];
 };
 
-static const struct level_data lev_data =
+static const struct t_level_data lev_data =
 {
     .blue_ship_n = 3,
-    .red_ship_n = 0,
+    .red_ship_n = 1,
 
     .blue_ships =
     {
-        {.pos = {.x = 5, .y = 2}, .type = SHIP_TYPE_SCOUT, .directions = {false, false, false, false}},
-        {.pos = {.x = 5, .y = 7}, .type = SHIP_TYPE_WARSHIP, .directions = {false, false, false, false}},
-        {.pos = {.x = 5, .y = 10}, .type = SHIP_TYPE_CARRIER, .directions = {false, false, false, false}},
+        {.pos = {.x = 10, .y = 2}, .type = SHIP_TYPE_SCOUT, .directions = {false, false, false, false}},
+        {.pos = {.x = 8, .y = 15}, .type = SHIP_TYPE_WARSHIP, .directions = {false, false, false, false}},
+        {.pos = {.x = 15, .y = 23}, .type = SHIP_TYPE_CARRIER, .directions = {false, false, false, false}},
         {.pos = {.x = 0, .y = 0}, .type = SHIP_TYPE_NONE, .directions = {false, false, false, false}},
         {.pos = {.x = 0, .y = 0}, .type = SHIP_TYPE_NONE, .directions = {false, false, false, false}},
         {.pos = {.x = 0, .y = 0}, .type = SHIP_TYPE_NONE, .directions = {false, false, false, false}},
@@ -60,7 +58,7 @@ static const struct level_data lev_data =
 
     .red_ships =
     {
-        {.pos = {.x = 0, .y = 0}, .type = SHIP_TYPE_NONE, .directions = {false, false, false, false}},
+        {.pos = {.x = 200, .y = 10}, .type = SHIP_TYPE_SCOUT, .directions = {false, false, true, false}},
         {.pos = {.x = 0, .y = 0}, .type = SHIP_TYPE_NONE, .directions = {false, false, false, false}},
         {.pos = {.x = 0, .y = 0}, .type = SHIP_TYPE_NONE, .directions = {false, false, false, false}},
         {.pos = {.x = 0, .y = 0}, .type = SHIP_TYPE_NONE, .directions = {false, false, false, false}},
