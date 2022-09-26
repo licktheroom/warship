2c9904e6c1bb37bae5751f2bcc9fd3de4230a382
licktheroom <77417158+licktheroom@users.noreply.github.com>
2022-09-25 20:02:33 -0400

    Update fragment.h

--- include/shaders/fragment.h
+++ include/shaders/fragment.h
@@ -4,19 +4,24 @@
 #define FRAGMENT_SHADER_HEADER
 
 static const char * FRAGMENT_shader =
+"// Copyright (C) 2022  licktheroom //\n"
+"\n"
 "#version 330 core\n"
 "out vec4 FragColor;\n"
 "\n"
-"uniform int color;\n"
+"in float col;\n"
 "\n"
 "void main()\n"
 "{\n"
-"    if(color == 0)\n"
+"    if(col == 0)\n"
 "        FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
-"    else if(color == 1)\n"
+"    else if(col == 1)\n"
 "        FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);\n"
-"    else\n"
+"    else if(col == 2)\n"
 "        FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+"    else\n"
+"        FragColor = vec4(0.0f, 0.0f, 0.5f, 1.0f);\n"
 "}\0";
+
 
 #endif
