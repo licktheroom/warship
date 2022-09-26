467d7b1de7b26a332db0335d9ae5f7e745fde675
licktheroom <77417158+licktheroom@users.noreply.github.com>
2022-09-25 20:04:21 -0400

    Update vertex.h

--- include/shaders/vertex.h
+++ include/shaders/vertex.h
@@ -6,19 +6,24 @@
 // that's why there's a plain text version
 
 static const char * VERTEX_shader =
+"// Copyright (C) 2022  licktheroom //\n"
+"\n"
 "#version 330 core\n"
 "layout (location = 0) in vec2 aPos;\n"
 "\n"
 "uniform ivec2 scn;\n"
-"uniform ivec2 pos[1500];\n"
+"uniform ivec3 pos[4001];\n"
 "\n"
 "uniform float scale;\n"
+"\n"
+"out float col;\n"
 "\n"
 "void main()\n"
 "{\n"
 "    vec2 finished = aPos;\n"
 "\n"
-"    finished += pos[gl_InstanceID];\n"
+"    finished.x += pos[gl_InstanceID].x;\n"
+"    finished.y += pos[gl_InstanceID].y;\n"
 "\n"
 "    finished.x *= scale;\n"
 "    finished.y *= scale;\n"
@@ -28,9 +33,9 @@ static const char * VERTEX_shader =
 "    finished.x = -1 + finished.x;\n"
 "    finished.y = 1 - finished.y;\n"
 "\n"
+"    col = pos[gl_InstanceID].z;\n"
+"\n"
 "    gl_Position = vec4(finished, 0.0f, 1.0f);\n"
 "}\0";
-
-
 
 #endif
